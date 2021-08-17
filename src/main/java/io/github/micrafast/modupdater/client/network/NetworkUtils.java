281a72d8cfd1fa511bb69f86940ed5165681f243
Micrafast <1191069521@qq.com>
2021-08-17 22:53:23 +0800

    Removed ignoring SSL certification

--- src/main/java/io/github/micrafast/modupdater/client/network/NetworkUtils.java
+++ src/main/java/io/github/micrafast/modupdater/client/network/NetworkUtils.java
@@ -7,13 +7,10 @@ import org.apache.http.HttpResponse;
 import org.apache.http.client.ClientProtocolException;
 import org.apache.http.client.ResponseHandler;
 import org.apache.http.client.methods.HttpGet;
-import org.apache.http.conn.ssl.NoopHostnameVerifier;
 import org.apache.http.impl.client.CloseableHttpClient;
 import org.apache.http.impl.client.HttpClients;
-import org.apache.http.ssl.SSLContexts;
 import org.apache.http.util.EntityUtils;
 
-import javax.net.ssl.SSLContext;
 import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
@@ -21,24 +18,7 @@ import java.io.OutputStream;
 
 public class NetworkUtils {
     private static Log log = LogFactory.getLog(NetworkUtils.class);
-    static CloseableHttpClient httpClient;
-
-    static {
-        httpClient = HttpClients.createDefault();
-        try {
-            SSLContext ctx = SSLContexts.custom()
-                    .loadTrustMaterial(null, (chain, authType) -> true)
-                    .build();
-            CloseableHttpClient prevClient = httpClient;
-            httpClient = HttpClients.custom()
-                    .setSSLContext(ctx)
-                    .setSSLHostnameVerifier(new NoopHostnameVerifier())
-                    .build();
-            prevClient.close();
-        } catch (Exception e) {
-            log.error("SSL Certification load failed", e);
-        }
-    }
+    static CloseableHttpClient httpClient = HttpClients.createDefault();
 
     public static String getString(String url) throws IOException {
         HttpGet get = new HttpGet(url);
